c0cd10fbbaa7013a46386a4726846db118ef3eef
harvested <andrei_doanca@yahoo.com>
2014-12-26 17:14:48 +0100

    Modified for new ROse::DB::Object #2

--- lib/MyIMDB/Models/Actors.pm
+++ lib/MyIMDB/Models/Actors.pm
@@ -3,9 +3,14 @@ package MyIMDB::Models::Actors;
 use strict;
 use warnings;
 
-use base 'MyIMDB::Models::Base';
+use base 'MyIMDB::Models::Object';
 
-__PACKAGE__->set_up_table('actors');
-__PACKAGE__->has_many( movies => 'MyIMDB::Models::UsersActors' );
+__PACKAGE__->meta->setup(
+    table      => 'actors',
+    columns    => [ qw(actor_id first_name last_name date_of_birth) ],
+    pk_columns => 'actor_id',
+    unique_key => 'actor_id',
+);
 
+# __PACKAGE__->has_many( movies => 'MyIMDB::Models::UsersActors' ); 
 1;
