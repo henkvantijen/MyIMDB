19a3117608552a80afd8cbf27d1cdcd356e41a67
harvested <andrei_doanca@yahoo.com>
2014-12-26 19:39:17 +0100

    issue #2

--- lib/MyIMDB/Models/Genres.pm
+++ lib/MyIMDB/Models/Genres.pm
@@ -3,10 +3,15 @@ package MyIMDB::Models::Genres;
 use strict;
 use warnings;
 
-use base 'MyIMDB::Models::Base';
+use base 'MyIMDB::Models::Object';
 
-__PACKAGE__->set_up_table('genres');
+__PACKAGE__->meta->setup(
+    table      => 'genres',
+    columns    => [ qw(genre_id genre) ],
+    pk_columns => 'genre_id',
+    unique_key => 'genre_id',
+);
 
-__PACKAGE__->has_many( movies => 'MyIMDB::Models::MoviesGenres' );
+#__PACKAGE__->has_many( movies => 'MyIMDB::Models::MoviesGenres' );
 
 1;
