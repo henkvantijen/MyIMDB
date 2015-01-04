78100c3a8711ea00a7eb5c1d09e2647114677310
harvested <andrei_doanca@yahoo.com>
2015-01-04 18:41:16 +0100

    Renamed Actors.pm to Actor.pm (singular) #2

---
+++ lib/MyIMDB/Models/Actor.pm
@@ -0,0 +1,18 @@
+package MyIMDB::Models::Actor;
+
+use strict;
+use warnings;
+
+use base 'MyIMDB::Models::Object';
+
+__PACKAGE__->meta->setup(
+    table      => 'actors',
+    columns    => [ qw(actor_id first_name last_name date_of_birth) ],
+    pk_columns => 'actor_id',
+    unique_key => 'actor_id',
+);
+
+__PACKAGE__->meta->make_manager_class('actors');
+
+# __PACKAGE__->has_many( movies => 'MyIMDB::Models::UsersActors' ); 
+1;
