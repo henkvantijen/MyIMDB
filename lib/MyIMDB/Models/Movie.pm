6bf4adfaf12d1f326aea430ae7d7b29e895bc3db
harvested <andrei_doanca@yahoo.com>
2015-01-04 20:07:14 +0100

    renamed Movies.pm to Movie.pm #2

---
+++ lib/MyIMDB/Models/Movie.pm
@@ -0,0 +1,21 @@
+package MyIMDB::Models::Movies;
+
+use strict;
+use warnings;
+
+use base 'MyIMDB::Models::Object';
+
+__PACKAGE__->meta->setup(
+    table      => 'movies',
+    columns    => [ qw(movie_id name launch_date duration rating) ],
+    pk_columns => 'movie_id',
+    unique_key => 'movie_id',
+);
+
+
+# do not remove until the setup is fully updated
+#__PACKAGE__->has_many( genres => 'MyIMDB::Models::MoviesGenres' );
+#__PACKAGE__->has_many( users => 'MyIMDB::Models::UsersMovies' );
+#__PACKAGE__->has_many( users_comments => 'MyIMDB::Models::MoviesUsersComments' );
+
+1;
